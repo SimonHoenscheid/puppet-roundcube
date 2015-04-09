38f1b7e2cfc0803065c66ab1132264c65749609a
Markus Frosch <markus@lazyfrosch.de>
2015-04-09 17:24:39 +0200

    Re-implement a simple apache config handling

---
+++ manifests/apache.pp
@@ -0,0 +1,29 @@
+# == Class: roundcube::apache
+#
+# Configure the Apache config file for roundcube
+#
+class roundcube::apache {
+
+  if ! defined(Class['::apache']) {
+    warn('Declaring Apache myself, you should do that - with prefork enabled!')
+    class { '::apache':
+      mpm_module => 'prefork',
+    }
+  }
+  include ::apache
+  include ::apache::mod::actions
+  include ::apache::mod::php
+  include ::apache::mod::mime
+  include ::apache::mod::deflate
+
+  ::apache::custom_config { 'roundcube':
+    source => "${::roundcube::confdir}/apache.conf",
+  }
+  ::apache::custom_config { 'roundcube-alias':
+    content => "# Managed by Puppet
+      Alias /roundcube/program/js/tiny_mce/ /usr/share/tinymce/www/
+      Alias /roundcube /var/lib/roundcube
+    ",
+  }
+
+}
